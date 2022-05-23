83cdb4b054de82f49a8f4485297900c606522053
VaibhavDwivedi-321 <86218655+VaibhavDwivedi-321@users.noreply.github.com>
2022-05-23 06:36:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var superheroes = require("superheroes");
+var mySuperhero = superheroes.random();
+var superVillains = require("supervillains");
+var mySuperVillain = superVillains.random();
+console.log(mySuperhero);
+console.log(mySuperVillain);
